7411dc7ebf8cd016fb7c08c0801fa4b4c9e95af0
Stilledood <corciova.f@gmail.com>
2023-02-13 19:16:54 +0200

    Added functionality for react to handle(save and display) reposts

--- postme/stories-web/src/stories/components.js
+++ postme/stories-web/src/stories/components.js
@@ -170,12 +170,12 @@ export function StoryParent(props){
   return story.original_story ? <div className='row'>
     <div className='col-11 mx-auto  p-3 border rounded'>
     <p className='mb-0 text-muted small'>Repost</p>
-    <Story className={' '} story={story.original_story}/> 
+    <Story hideActions  className={' '} story={story.original_story}/> 
     </div>
   </div> : null
 }
 export function Story(props){
-  const {story,didRepost} = props;
+  const {story,didRepost,hideActions} = props;
   const className = props.className ? props.className:'col-10 mx-auto col-md-8';
   const [actionStory,setActionStory] = useState(props.story ? props.story:null);
   const handlePerformAction = (newActionStory,status) =>{
@@ -196,7 +196,7 @@ export function Story(props){
 
         </div>
         
-        {actionStory && <div className='btn btn-group'>
+        {(actionStory && hideActions !== true) && <div className='btn btn-group'>
           <ActionBtn story={actionStory} didPerformAction={handlePerformAction} action={{type:"like",display:"Likes"}}></ActionBtn>
           <ActionBtn story={actionStory} didPerformAction={handlePerformAction} action={{type:"Unlike",display:"Unlike"}}></ActionBtn>
           <ActionBtn story={actionStory} didPerformAction={handlePerformAction} action={{type:"repost",display:"Repost"}}></ActionBtn>
