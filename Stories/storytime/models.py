5ac9688c94a7644fdd57eeb7479c716d45852f87
Stilledood <corciova.f@gmail.com>
2023-02-21 19:21:02 +0200

    Added javascript functionality to display an error message for story create form

--- Stories/storytime/models.py
+++ Stories/storytime/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
 
 class Story(models.Model):
 
     content = models.TextField(blank=True,null=True)
     image = models.FileField(blank=True,null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content[:10]
 
     class Meta:
         ordering = ['-pk']
