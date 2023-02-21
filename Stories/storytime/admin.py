c06a0f09465f1721974293befbe1de179e296b9f
Stilledood <corciova.f@gmail.com>
2023-02-21 19:47:57 +0200

    Created custom admin model for Story objects

--- Stories/storytime/admin.py
+++ Stories/storytime/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Story
 
-admin.site.register(Story)
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    search_fields = ['user__username','user__email']
+    class Meta:
+        model = Story
+
+
 
