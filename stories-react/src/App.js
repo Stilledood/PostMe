3e3e5f0d800ec485fa59e64610abe46bdd1b81b9
Stilledood <corciova.f@gmail.com>
2023-03-01 19:59:06 +0200

    Added functional components in React

--- stories-react/src/App.js
+++ stories-react/src/App.js
@@ -2,35 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import React ,{useEffect,useState} from 'react';
 
-import {Story} from "./stories"
+import {StoriesList} from "./stories"
 
-function loadStories(callback){
-  const xhr = new XMLHttpRequest()
-  const method = "GET"
-  const endpoint = "http://127.0.0.1:8000/stories/"
-  const responseType = "json"
 
-  xhr.responseType = responseType
-  xhr.open(method,endpoint)
-  xhr.onload = function () {
-      callback(xhr.response,xhr.status)
-  }
-  xhr.send()
-}
+
 
 
 
 function App() {
-  const [stories,setStories] = useState([])
   
-  useEffect(() => {
-    const myCallback = (response,status) =>{
-      setStories(response)
-      
-    }
-    loadStories(myCallback)
-
-  },[])
   return (
     <div className="App">
       <header className="App-header">
@@ -39,9 +19,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          {stories.map((item,index) => {
-            return <Story story={item} key={index} className="my-5 py-4 border bg-white text-dark" />
-          })}
+          <StoriesList />
         </div>
         <a
           className="App-link"
--- stories-react/src/stories/components.js
+++ stories-react/src/stories/components.js
@@ -1,4 +1,35 @@
-import React from "react";
+import React ,{useEffect,useState} from 'react';
+
+
+
+export function StoriesList (props){
+    const [stories,setStories] = useState([])
+    useEffect(() => {
+      const myCallback = (response,status) =>{
+        setStories(response)
+      }
+      loadStories(myCallback)
+    },[])
+  
+    return stories.map((item,index) => {
+      return <Story story={item} key={index} className="my-5 py-4 border bg-white text-dark" />
+    })
+  
+}
+  
+  function loadStories(callback){
+    const xhr = new XMLHttpRequest()
+    const method = "GET"
+    const endpoint = "http://127.0.0.1:8000/stories/"
+    const responseType = "json"
+  
+    xhr.responseType = responseType
+    xhr.open(method,endpoint)
+    xhr.onload = function () {
+        callback(xhr.response,xhr.status)
+    }
+    xhr.send()
+  }
 
 
 export function ActionBtn(props){
