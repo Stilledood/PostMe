29655f7794cb6a712a27e27b1973f90c5f32ec68
Stilledood <corciova.f@gmail.com>
2023-03-01 19:10:24 +0200

    Added React like button

--- stories-react/src/App.js
+++ stories-react/src/App.js
@@ -16,11 +16,20 @@ function loadStories(callback){
   xhr.send()
 }
 
+function ActionBtn(props){
+  const {story,action} = props
+  const className = props.className ? props.className : "btn btn-primary btn-sm"
+  return action.type === 'like' ? <button className={className} >{story.likes} Likes</button>:null
+}
+
 function Story(props) {
   const {story} = props
   const className = props.className ? props.className: "col-10 mx-auto cold-md-6"
   return <div className={className}>
     <p>{story.id} - {story.content}</p>
+    <div className="btn btn-group">
+      <ActionBtn story={story} action={{type:"like"}} />
+    </div>
   </div>
 }
 
